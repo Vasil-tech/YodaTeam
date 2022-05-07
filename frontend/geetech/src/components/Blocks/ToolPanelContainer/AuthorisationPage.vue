3c73924589cc02b7622899209bc863b028d842cb
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-05-07 15:46:25 +0300

    debug forms

--- frontend/geetech/src/components/Blocks/ToolPanelContainer/AuthorisationPage.vue
+++ frontend/geetech/src/components/Blocks/ToolPanelContainer/AuthorisationPage.vue
@@ -44,34 +44,33 @@ export default {
     methods:{
         showAuth(){
         if(this.way == "Registration"){
-        this.RegistForm = true;
-        this.AuthForm = false
-            }
+            this.RegistForm = true;
+            this.AuthForm = false
+        }
         else if(this.way == "Authorisation"){
             this.AuthForm = true;
             this.RegistForm = false
-            }
+        }
         },
     fieldAlertCheck(){
-        if(this.way == "Authorisation" && document.getElementById("loginInput").value.length < 3 || document.getElementById("passwordInput").value.length <= 8){
+        if(document.getElementById("loginInput").value.length < 3 || document.getElementById("passwordInput").value.length <= 8){
             this.fieldAlert = true;
         }
-        else{
+        else if(this.way == "Authorisation"){
             this.fieldAlert = false
             jsonGenerate({ action: this.way, login: document.getElementById("loginInput").value, password: document.getElementById("passwordInput").value})
             return null;
         }
-        if(this.way == "Registration" && document.getElementById("emailInput").value.length < 5 || document.getElementById("loginInput").value.length < 3 || document.getElementById("passwordInput").value.length <= 8){
+        if(document.getElementById("emailInput").value.length <=  5 || document.getElementById("loginInput").value.length <= 3 || document.getElementById("passwordInput").value.length <= 8){
             this.fieldAlert = true;
         }
-        else{
+        else if(this.way == "Registration"){
             this.fieldAlert = false
             jsonGenerate({action: this.way, email: document.getElementById("emailInput").value, login: document.getElementById("loginInput").value, password: document.getElementById("passwordInput").value})
         }
     }
     }
 }
-
 </script>
 
 <style>
