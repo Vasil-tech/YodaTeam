9fd2173db82fea4022790fae57061e57c63b81ed
Neuromancer404 <neuromancer@gmail.com>
2022-05-13 17:27:16 +0300

    xmlrequest

--- frontend/geetech/src/components/Blocks/scripts/JSONgenerate.js
+++ frontend/geetech/src/components/Blocks/scripts/JSONgenerate.js
@@ -1,25 +1,22 @@
-//import { emitter } from "@/main";
+import { emitter } from "@/main";
 import errorHandler from "./errorHandler";
-import $ from "ajax"
 export default function(obj){
     try{
         obj = JSON.stringify(obj)
         let check = IsJsonString(obj)
         if(check){
-            console.log(window.location.pathname)
-            $.ajax({
-                type : "POST",
-                contentType : "application/json; charset=utf-8",
-                url : window.location.pathname,
-                data : obj,
-                complete: function () {
-                    // По завершению
-                    $("#titleId").val("");
-                    $("#textId").val("");
-                    //emitter.emit("Responce", "a")
-                    console.log(" request: " )
+            let xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('POST', "/", true);
+            xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xmlhttp.send("biba")
+            xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if(this.status == 200){
+                    console.log(this.responseText);
+                    emitter.emit("Response", this.responseText)
+                    }
                 }
-            });
+            }
         }
         else{
             errorHandler("JSONgenerate", "default", "not json", "Connection")
@@ -34,6 +31,7 @@ export default function(obj){
 function IsJsonString(str) {
     try {
         JSON.parse(str);
+        console.log(str)
     } catch (e) {
         return false;
     }
